d8c7b7a7dba28a6fb92ab48a05848b2110b96eaf
Alex.Vale <alex.vale@enable.com>
2024-12-16 11:44:09 +0000

    Change to parsing eds components

--- script.js
+++ script.js
@@ -1,3 +1,178 @@
+const COMPONENTS = [
+    {
+        name: "Accordion",
+        tag: "EDS-ACCORDION"
+    },
+    {
+        name: "Auto Fill Search (deprecated)",
+        tag: "EDS-AUTO-FILL-SEARCH"
+    },
+    {
+        name: "Avatar",
+        tag: "EDS-AVATAR"
+    },
+    {
+        name: "Bar Chart",
+        tag: "EDS-BAR-CHART"
+    },
+    { 
+        name: "Button", 
+        tag: "EDS-BUTTON" 
+    },
+    {
+        name: "Combo Chart",
+        tag: "EDS-CHART"
+    },
+    {
+        name: "Date Picker",
+        tag: "EDS-DATE-PICKER"
+    },
+    {
+        name: "Doughnut Chart",
+        tag: "EDS-DOUGHNUT-CHART"
+    },
+    {
+        name: "Dot",
+        tag: "EDS-DOT"
+    },
+    {
+        name: "Dropdown (deprecated)",
+        tag: "EDS-DROPDOWN"
+    },
+    {
+        name: "File Attachment",
+        tag: "EDS-FILE-ATTACHMENT"
+    },
+    {
+        name: "File Upload",
+        tag: "EDS-FILE-UPLOAD"
+    },
+    {
+        name: "Filter (deprecated)",
+        tag: "EDS-FILTER"
+    },
+    {
+        name: "Form Field",
+        tag: "EDS-FORM-FIELD"
+    },
+    {
+        name: "Help Text",
+        tag: "EDS-HELP-TEXT"
+    },
+    {
+        name: "Hub Switcher",
+        tag: "EDS-HUB-SWITCHER"
+    },
+    { 
+        name: "Input", 
+        tag: "EDS-INPUT" 
+    },
+    {
+        name: "Input Range",
+        tag: "EDS-INPUT-RANGE"
+    },
+    {
+        name: "Line Chart",
+        tag: "EDS-LINE-CHART"
+    },
+    { 
+        name: "Loader", 
+        tag: "EDS-LOADER" 
+    },
+    {
+        name: "Modal",
+        tag: "EDS-MODAL"
+    },
+    {
+        name: "Multi Select",
+        tag: "EDS-MULTI-SELECT"
+    },
+    {
+        name: "Pager",
+        tag: "EDS-PAGER"
+    },
+    { 
+        name: "Pill", 
+        tag: "EDS-PILL" 
+    },
+    {
+        name: "Popover",
+        tag: "EDS-POPOVER"
+    },
+    {
+        name: "Profile Menu",
+        tag: "EDS-PROFILE-MENU"
+    },
+    { 
+        name: "Radio", 
+        tag: "EDS-RADIO" 
+    },
+    {
+        name: "Resizing Textarea",
+        tag: "EDS-RESIZING-TEXTAREA"
+    },
+    {
+        name: "Search",
+        tag: "EDS-SEARCH"
+    },
+    {
+        name: "Selector Button",
+        tag: "EDS-SELECTOR-BUTTON"
+    },
+    { 
+        name: "Select", 
+        tag: "EDS-SELECT" 
+    },
+    {
+        name: "Single Select",
+        tag: "EDS-SINGLE-SELECT"
+    },
+    { 
+        name: "Spacing", 
+        tag: "EDS-SPACING" 
+    },
+    { 
+        name: "Stepper", 
+        tag: "EDS-STEPPER" 
+    },
+    { 
+        name: "Table", 
+        tag: "EDS-TABLE" 
+    },
+    { 
+        name: "Tag", 
+        tag: "EDS-TAG" 
+    },
+    { 
+        name: "Textarea", 
+        tag: "EDS-TEXTAREA" 
+    },
+    { 
+        name: "Tile", 
+        tag: "EDS-TILE" 
+    },
+    { 
+        name: "Tile Grid", 
+        tag: "EDS-TILE-GRID" 
+    },
+    {
+        name: "Tile Grid Dial Pad",
+        tag: "EDS-TILE-GRID--DIAL-PAD"
+    },
+    { 
+        name: "Toast", 
+        tag: "EDS-TOAST" 
+    },
+    {
+        name: "Toggle Button",
+        tag: "EDS-TOGGLE-BUTTON"
+    },
+    {
+        name: "Toggle Switch",
+        tag: "EDS-TOGGLE-SWITCH"
+    }
+];
+
 const state = {
     totalElements: 0,
     edsElements: 0,
@@ -14,8 +189,10 @@ function traverseDOM(node) {
 
         state.totalElements++;
 
-        if (typeof node.className === 'string' && node.className.includes('eds')) {
+        if (typeof node.className === 'string' && nodeIsEdsComponent(node)) {
             state.edsElements++;
+            console.log(node.nodeName);
+            return;
         }
 
         node.childNodes.forEach(traverseDOM);
@@ -25,6 +202,10 @@ function traverseDOM(node) {
 function nodeIrrellevant(node) {
     const irrelevantNodeNames = ['HEAD', 'SCRIPT', 'STYLE', 'IFRAME', 'SVG'];
     return irrelevantNodeNames.includes(node.nodeName);
+}
+
+function nodeIsEdsComponent(node) {
+    return COMPONENTS.some(component => node.nodeName === component.tag);
 }
 
 function createResultsPanel() {
