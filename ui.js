8096e2267f41c97e07bf8fec932acb3e0f12401f
Alex.Vale <alex.vale@enable.com>
2024-12-17 12:26:51 +0000

    Add buttons

--- ui.js
+++ ui.js
@@ -16,8 +16,18 @@ function init() {
         <div class="eds-snap">
             <header class="eds-snap__header" id="panel-header">
                 <img src="${logoUrl}" alt="EDS Logo" id="snapPanelLogo">
-                <button id="runSnapButton">Run Snap</button>
-                <button id="unSnapButton">Unsnap</button>
+                <div class="button-bar">
+                    <button id="runSnapButton">
+                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M16.023 9.34841H21.0156V9.34663M2.98438 19.6444V14.6517M2.98438 14.6517L7.97702 14.6517M2.98438 14.6517L6.16527 17.8347C7.15579 18.8271 8.41285 19.58 9.8646 19.969C14.2657 21.1483 18.7895 18.5364 19.9687 14.1353M4.03097 9.86484C5.21024 5.46374 9.73402 2.85194 14.1351 4.03121C15.5869 4.4202 16.8439 5.17312 17.8345 6.1655L21.0156 9.34663M21.0156 4.3558V9.34663" stroke="#1A5919" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                        </svg>
+                    </button>
+                    <button id="close-button">
+                        <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                          <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+                        </svg>
+                    </button>
+                </div>
             </header>
             <div class="eds-snap__content" id="eds-snap-panel-content">
               <h1 id="eds-snap-title" class="eds-snap__title"></h1>
